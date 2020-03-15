0854fe4ce8ace6abf6dc0bbcf71984595ff6d42a
Erkka Rahikainen <ergga96@gmail.com>
2020-03-15 22:07:12 +0200

    asd

--- hy-data-analysis-with-python-spring-2020/part01-e06_triple_square/src/triple_square.py
+++ hy-data-analysis-with-python-spring-2020/part01-e06_triple_square/src/triple_square.py
@@ -2,7 +2,17 @@
 
 
 def main():
-    pass
+    for i in range(1,11):
+        tripled = triple(i)
+        squared = square(i)
+        if squared > tripled:
+            break
+        print(f"triple({i})=={tripled} square({i})=={squared}")
+
+def triple(value):
+    return value*3
+def square(value):
+    return value**2
 
 if __name__ == "__main__":
     main()
